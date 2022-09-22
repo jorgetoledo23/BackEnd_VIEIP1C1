103408ccb8d1c9b2233940523105c8c3664e342a
Jorge <jorge.toledo.6@hotmail.com>
2022-09-22 20:22:00 -0300

    EF

---
+++ ConsoleAppEntityFramework/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ 
+    Generar CRUD completo para una tabla Productos
+
+    Los campos de la tabla Productos deben ser:
+    -Id
+    -Name
+    -Precio
+    -Stock
+    -UrlImagen
+
+    Poder:
+
+    -Insertar nuevos Productos
+    -Ver Productos
+    -Actualizar
+    -Eliminar
+
+    Buscador de Productos
+
+
+ */
