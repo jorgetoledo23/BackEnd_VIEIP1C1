2b7d1c106b1abfe5087ee461745a5c9518c27cde
Jorge <jorge.toledo.6@hotmail.com>
2022-10-20 20:35:42 -0300

    MVC

---
+++ Sistema Web MVC/Models/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_Web_MVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+    }
+
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public string Desripcion { get; set; }
+        public int Stock { get; set; }
+        public int Precio { get; set; }
+        public string UrlImagen { get; set; }
+
+        public int CategoriaId { get; set; }
+    }
+
+    public class Categoria
+    {
+        public int CategoriaId{ get; set; }
+        public string Nombre{ get; set; }
+        public string Descripcion{ get; set; }
+    }
+}
